d44968f61f503c9674c7fe3d388b52843d991244
hj60490 <harshj60490@gmail.com>
2022-11-07 09:43:00 +0530

    add cart item component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import React from "react";
+import CartItem from "./CartItem";
 
 function App() {
   return (
     <div className="App">
      <h1>Harsh</h1>
+     <CartItem />
     </div>
   );
 }
